2a8c2d89e8cf4cc61f59d05d67930c049bf5fb9e
Luis Gonzalez <otfgonzalez@gmail.com>
2025-08-10 20:49:36 -0700

    Added gRPC to server-three

---
+++ apps/server-three/app.py
@@ -0,0 +1,36 @@
+import asyncio, psutil, grpc
+from concurrent import futures
+import service_pb2 as pb
+import service_pb2_grpc as rpc
+from verse.common import get_memory_usage
+from grpc_reflection.v1alpha import reflection
+
+
+class ServerThree(rpc.VerseServiceServicer):
+    async def Echo(self, request, context):
+        return pb.EchoReply(msg=request.msg)
+
+    async def GetMemory(self, request, context):
+        return pb.MemReply( usage=get_memory_usage() )
+    
+
+async def serve():
+    server = grpc.aio.server()
+    rpc.add_VerseServiceServicer_to_server(ServerThree(), server)
+
+    SERVICE_NAMES = (
+        'demo.v1.VerseService',
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server=server)
+
+    server.add_insecure_port("0.0.0.0:5000")
+    await server.start()
+    print("gRPC server running on port 5000")
+    await server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    asyncio.run(serve())
+
+
